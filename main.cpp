0d0c6878bf4e22ae2ef843997b9351d4ddd317b9
Ivan Korolkov <korolkov.ivan@mail.ru>
2023-01-03 21:41:08 +0300

    create class Complex

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+class Complex
+{
+private:
+	double re = 0.0;
+	double im = 0.0;
+public:
+	Complex(double r, double i)
+		: re(r)
+		, im(i)
+	{}
+	Complex(double r)
+		: re(r)
+	{}
+};
+
 int main()
 {
 	return 0;
